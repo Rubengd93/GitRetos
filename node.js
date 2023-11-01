8ce3531a93203b7e87297c0e7dc3cb803c7df5fa
Rubengd93 <rubengd93@gmail.com>
2023-11-01 20:29:36 +0100

    añadir funcion

---
+++ node.js
@@ -0,0 +1,34 @@
+
+function calculator(operator,num1, num2) {
+
+    if (operator === "sum") {
+          
+          return num1 + num2;
+
+    }
+    if (operator === "subs") {
+          
+           return num1 - num2;
+
+     }
+     if (operator === "mult") {
+          
+           return num1 * num2;
+
+     }
+     if (operator === "div") {
+          
+           return num1 / num2;
+
+     }
+
+}
+
+function cuadNum(n){
+
+    return n*2;
+
+}
+
+// let cuad2 = cuadNum(4);
+// console.log(cuad2);
